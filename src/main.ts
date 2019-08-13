ff3eba5caee4d8cb9852e57eb33ed393923cacb7
Philip Chwistek <mrsportsnerd@gmail.com>
2019-08-13 00:42:37 -0400

    set up initial stuff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as helmet from 'helmet'
+import * as morgan from 'morgan'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.enableCors()
+  app.use(helmet())
+  app.use(morgan('dev'))
+
+  await app.listen(3009)
+  console.log('====== Listening at port 3009 ======')
 }
-bootstrap();
+bootstrap()
