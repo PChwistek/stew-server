5e8eccaf5d95e68463f0d5563ee6d4fba060e708
Philip Chwistek <mrsportsnerd@gmail.com>
2019-11-29 02:56:27 -0800

    fixed some cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as morgan from 'morgan'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
+  app.enableCors({credentials: true, origin: 'http://localhost:3000'})
   app.useGlobalPipes(new ValidationPipe())
   app.use(helmet())
   app.use(morgan('dev'))
